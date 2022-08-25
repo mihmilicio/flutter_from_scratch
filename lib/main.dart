1b56edd88563dff085b05a3830526abd5b1e7f9b
Milena Milicio <milenamilicio@hotmail.com>
2022-08-25 20:35:53 -0300

    Quarta versão: refatoração StatelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,61 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Container(
-    padding: const EdgeInsets.only(top: 62),
-    decoration: const BoxDecoration(color: Colors.white),
-    child: Column(
-      children: [
-        Container(
-          height: 50,
-          decoration: const BoxDecoration(color: Colors.purple),
-          child: Row(
+  runApp(const AppDoZero());
+}
+
+class AppDoZero extends StatelessWidget {
+  const AppDoZero({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 62),
+      decoration: const BoxDecoration(color: Colors.white),
+      child: Column(
+        children: const [TitleBar(), HomePage()],
+      ),
+    );
+  }
+}
+
+class TitleBar extends StatelessWidget {
+  const TitleBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      decoration: const BoxDecoration(color: Colors.purple),
+      child: Row(
+        textDirection: TextDirection.ltr,
+        children: const [
+          Icon(
+            Icons.menu,
             textDirection: TextDirection.ltr,
-            children: const [
-              Icon(
-                Icons.menu,
-                textDirection: TextDirection.ltr,
-              ),
-              Text(
-                'Primeiro app',
-                textDirection: TextDirection.ltr,
-              )
-            ],
           ),
-        ),
-        Image.asset('lib/img/krobus.png'),
-        const Expanded(
-            child: Center(
-                child: Text(
-          'App sem material',
-          textDirection: TextDirection.ltr,
-          style: TextStyle(
-              color: Colors.black, fontSize: 32, fontWeight: FontWeight.w500),
-        )))
-      ],
-    ),
-  ));
+          Text(
+            'Primeiro app',
+            textDirection: TextDirection.ltr,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Expanded(
+        child: Center(
+            child: Text(
+      'App sem material',
+      textDirection: TextDirection.ltr,
+      style: TextStyle(
+          color: Colors.black, fontSize: 32, fontWeight: FontWeight.w500),
+    )));
+  }
 }
