7f9eb7cd67a9d2b5076b307ac4d6e02e49de3d23
Milena Milicio <milenamilicio@hotmail.com>
2022-08-25 20:27:04 -0300

    Terceira versão: Column, Row, Icon, Image, Expanded

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,37 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(Container(
-    decoration: const BoxDecoration(
-      color: Colors.white
-    ),
-    child: const Center(
-      child: Text(
-        'App sem material',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 32,
-          fontWeight: FontWeight.w500
+    padding: const EdgeInsets.only(top: 62),
+    decoration: const BoxDecoration(color: Colors.white),
+    child: Column(
+      children: [
+        Container(
+          height: 50,
+          decoration: const BoxDecoration(color: Colors.purple),
+          child: Row(
+            textDirection: TextDirection.ltr,
+            children: const [
+              Icon(
+                Icons.menu,
+                textDirection: TextDirection.ltr,
+              ),
+              Text(
+                'Primeiro app',
+                textDirection: TextDirection.ltr,
+              )
+            ],
+          ),
         ),
-      ),
+        Image.asset('lib/img/krobus.png'),
+        const Expanded(
+            child: Center(
+                child: Text(
+          'App sem material',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+              color: Colors.black, fontSize: 32, fontWeight: FontWeight.w500),
+        )))
+      ],
     ),
   ));
 }
